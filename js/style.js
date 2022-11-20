866e4cd670df12f24fc14bc67a9138a8d362570f
LukyTechnology <lukytechnology@gmail.com>
2022-11-20 21:17:43 +0100

    Mobile Optimize

--- js/style.js
+++ js/style.js
@@ -2,3 +2,12 @@
 function notebook() {
     alert("Tento notebook sa už nepredáva!");
 }    
+
+/* MOBILNE MENU */
+const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', function () {
+  this.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+})
